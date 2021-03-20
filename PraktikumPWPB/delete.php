a8210e8efeb4f13b9652c7b5a7d1293edf092e31
Ukung1 <72364368+Ukung1@users.noreply.github.com>
2021-03-20 13:48:16 +0700

    Delete delete.php

--- PraktikumPWPB/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'lib/library.php';
-$nis = $_GET['nis'];
-
-$sql = "DELETE FROM siswa WHERE nis = $nis";
-
-$mysqli->query($sql) or die($mysqli->error);
-
-header('location: index.php');
