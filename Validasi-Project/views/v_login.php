77367155db7ed976ddeacd92a6b5c4d506ff4b29
Ukung1 <72364368+Ukung1@users.noreply.github.com>
2021-06-04 07:21:42 +0700

    Add files via upload

---
+++ Validasi-Project/views/v_login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vito Upload</title>
+    <link rel="stylesheet" type="text/css" href="assets/css.css">
+</head>
+<body>
+
+   <div class="logintabel">
+    <form class="form-horizontal" action="login.php" method="POST">
+    <h1 id="grad1">Login</h1>
+        <div class="form-group">
+            <label class="">Username</label>
+            <div class="">
+                <input type="text" name="username" value="">
+            </div>
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <div class="">
+                <input type="password" name="password" value="">
+            </div>
+        </div>
+        <div class="form-group">
+            <label></label>
+            <div class="">
+            <button type="submit">Login</button>
+            </div>
+        </div>
+    </form>
+    </div>
+</body>
+</html>
